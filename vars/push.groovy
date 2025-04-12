0588b7e7ed21d867ffc325cea7383955649eab1d
isthatyous <132775322+isthatyous@users.noreply.github.com>
2025-04-12 20:14:33 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,13 +1,13 @@
 def call(String credId , String IMAGE_NAME){
      withCredentials([usernamePassword(
-                    credentialsId: '${credId}',
+                    credentialsId: credId,
                     usernameVariable: 'DockerHubUser',
                     passwordVariable: 'DockerHubPass'
                     )]) {
                     
                     echo "Pushing Image to Docker Hub"
-                    sh "docker image tag ${IMAGE_NAME} ${env.DockerHubUser}/${IMAGE_NAME}"
                     sh "docker login -u ${env.DockerHubUser} -p ${env.DockerHubPass}"
+                    sh "docker image tag ${IMAGE_NAME} ${env.DockerHubUser}/${IMAGE_NAME}"
                     sh "docker push ${env.DockerHubUser}/${IMAGE_NAME}:latest"
                     echo "Image Pushed to Docker Hub Successfully"
 
